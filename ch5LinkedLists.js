3f606f555d3394bbf753d5e6ac58751c0f658982
Myhanhpham25 <myhanhpham25@gmail.com>
2018-02-20 00:03:35 -0800

    updated examples

--- ch5LinkedLists.js
+++ ch5LinkedLists.js
@@ -100,19 +100,168 @@ SinglyList.prototype.addFront = function(value){
     newNode.next = this.head;
     this.head = newNode; 
     return this; 
+};
+
+SinglyList.prototype.contains = function(){
+    if(!this.head){
+        return undefined;
+    }
+    return this.head.value;
+};
+
+SinglyList.prototype.sListLength = function(value){
+    var newNode = new Node(value)
+    if(!this.head){
+        this.head = newNode;
+        this._length++;
+        return this._length;
+    }else{
+        currentNode = this.head;
+        while(currentNode.next != null){
+            currentNode = currentNode.next; 
+        }
+        currentNode = newNode;
+        this._length++
+        return this._length;
+    }
+};
+
+SinglyList.prototype.sListDisplay = function(){
+    myList = []; 
+    if(!this.head){
+        return undefined;
+    }
+    currentNode = this.head;
+
+    while(currentNode){
+        myList.push(currentNode.value);
+        currentNode = currentNode.next;
+    }
+    console.log(myList);
+    console.log(myList.join(" "));
+    return myList;
+};
+
+SinglyList.prototype.sListMax = function(){
+    var max = this.head.value;
+    currentNode = this.head; 
+
+    while(currentNode){
+        if(currentNode.value > max){
+            max = currentNode.value;
+        }
+        currentNode = currentNode.next; 
+    }
+    return max; 
+};
+
+SinglyList.prototype.sListMin = function(){
+    currentNode = this.head 
+    var min = this.head.value; 
+
+    while(currentNode){
+        if(currentNode.value < min){
+            min = currentNode.value;
+        }
+        currentNode = currentNode.next;
+    }
+    return min;
+}; 
+
+SinglyList.prototype.sListAverage = function(){
+    var sum = 0; 
+    var count = 0; //if you don't already have length tracked
+    var length = this._length; 
+    currentNode = this.head; 
+
+    while(currentNode){
+        sum += currentNode.value; 
+        console.log(sum);
+        count++;
+        currentNode = currentNode.next;
+    }
+    var average = Math.floor(sum/length); 
+    return average; 
+};
+
+SinglyList.prototype.sListBack = function(){
+    currentNode = this.head; 
+
+    while(currentNode.next != null){
+        currentNode = currentNode.next;
+    }
+    return currentNode.value; 
+};
+
+SinglyList.prototype.sListRemoveBack = function(){
+    previousNode = this.head; 
+    thisNode = previousNode.next;
+
+
+    while(thisNode){
+        if(thisNode.next == null){
+            break;
+        }
+       previousNode = thisNode;
+       thisNode = thisNode.next;
+    }
+    previousNode.next = null; 
+    return this;    
+};
+
+SinglyList.prototype.sListAddBack = function(value){
+    var newNode = new Node(value);
+    if(!this.head){
+        this.head = newNode;
+        this._length++;
+        return newNode;
+    }else{
+        currentNode = this.head;
+        while(currentNode.next !=null){
+            currentNode = currentNode.next;
+        }
+        currentNode = newNode;
+        this._length++;
+        return this;
+    }
+};
+
+SinglyList.prototype.sListMoveMintoFront = function(){
+    var low = this.sListMin();
+    // console.log(low)
+    var front = this.addFront(low)
+    // console.log(front)
+    return this;
+};
+
+SinglyList.prototype.sListMoveMaxtoBack = function(){
+    var high = this.sListMax();
+    console.log(high);
+    var back = this.sListAddBack(high);
+    console.log(back);
+    return this; 
 }
 
 
 
-
 var sll = new SinglyList(); 
-sll.add(1);
-sll.add(25);
-sll.add(35);
 sll.add(4);
-sll.searchNodeAt(2);
-var x = sll.remove(25);
-
-// console.log(sll);
+sll.add("World")
+sll.add(3);
+sll.add(2);
+sll.add(7);
+sll.add("Hello");
+sll.add("ending")
+// sll.searchNodeAt(2);
+// sll.remove(25);
+// sll.sListLength(1);
+// sll.contains();
+var x = sll.sListMoveMaxtoBack();
+// sll.sListAddBack(8)
+// sll.sListRemoveBack();
+// sll.sListDisplay();
+// var y = sll.sListAverage();
+// sll.sListMin();
+// sll.sListMax();
 console.log(x);
 
